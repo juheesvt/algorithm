25d97fd39616859a81b3fbfd4cfb231905faa499
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2024-04-17 03:29:02 +0900

    [Silver IV 1158] Title: 요세푸스 문제, Time: 64 ms, Memory: 34016 KB -NKLCBHub

--- 백준/Silver/#1158 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/#1158 요세푸스 문제/요세푸스 문제.py
@@ -1,33 +1,15 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-josephus = [i for i in range(1, n + 1)]
-answers = []
+circle = deque([i for i in range(1, n+1)])
+seq = []
 
-count, idx = 0, 0
-while josephus:
-    count += 1
-    if count == k:
-        answers.append(josephus[idx])
-        if idx == len(josephus) - 1:
-            del josephus[idx]
-            idx = 0
-        else:
-            del josephus[idx]
-        count = 0
-    else:
-        idx += 1
-    
-    if idx == len(josephus):
-        idx = 0
+while circle:
+    circle.rotate(-k)
+    seq.append(circle.pop())
 
-answer = "<"
-for i in answers:
-    answer += str(i) + ", "
-else:
-    answer = answer[:-2]
-    answer += ">"
-print(answer)
+print('<' + ', '.join(map(str, seq)) + '>')

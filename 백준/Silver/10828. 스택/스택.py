61a596672a06ba03c1551d08cca6202259dbab57
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2024-08-06 22:55:50 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,28 +1,57 @@
 import sys
-n = int(sys.stdin.readline())
 
-stack = list()
+class Stack:
+   arr = list()
+   top = -1
 
-for i in range(n):
-   command = sys.stdin.readline().split()
+   def __init__(self):
+      self.arr = [ 0 for i in range(10000)]
+      self.top = -1
+       
+   def push(self, x: int) -> int:
+      self.top += 1
+      self.arr[self.top] = x
+
+   def pop(self) -> int:
+      if self.empty() == 1:
+         return -1
+
+      val = self.arr[self.top]
+      self.top -= 1
+      return val
    
-   # push
-   if len(command) > 1:
-      num = int(command[1])
-      stack.append(num)
+   def peek(self) -> int:
+      if self.empty() == 1:
+         return -1
+      return self.arr[self.top]
 
-   elif command[0] == "top":
-      print(stack[-1] if len(stack) else -1)
+   def empty(self) -> int:
+      return 1 if self.top == -1 else 0
 
-   elif command[0] == "size":
-      print(len(stack))
+   def size(self) -> int:
+      return self.top + 1
 
-   elif command[0] == "empty":
-      print(0 if len(stack) else 1)
 
-   elif command[0] == "pop":
-      if len(stack):
-         print(stack[-1])
-         del stack[-1]
-      else:
-         print(-1)
+if __name__ == "__main__":
+   n = int(sys.stdin.readline())
+
+   stack = Stack()
+
+   for i in range(n):
+      command = sys.stdin.readline().split()
+      
+      # push
+      if len(command) > 1:
+         stack.push(int(command[1]))
+
+      elif command[0] == "top":
+         print(stack.peek())
+
+      elif command[0] == "size":
+         print(stack.size())
+
+      elif command[0] == "empty":
+         print(stack.empty())
+
+      elif command[0] == "pop":
+         print(stack.pop())

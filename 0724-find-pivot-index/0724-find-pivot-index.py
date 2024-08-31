9b9e8f78be6cc4dabe0ec1543231522440b664ec
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2024-08-31 13:22:23 +0900

    Delete 0724-find-pivot-index/0724-find-pivot-index.py

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-
-        for idx in range(len(nums)):
-            if sum(nums[:idx]) == sum(nums[idx + 1:]):
-                return idx
-        return -1
-        

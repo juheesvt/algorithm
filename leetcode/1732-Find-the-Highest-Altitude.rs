e15928a5fe31ce76a72e45fd91fe8732df28a28d
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2024-09-13 19:41:00 +0900

    Title: Find the Highest Altitude, Time Beats: 100.00%, Memory Beats: 87.41% - LeetPush

---
+++ leetcode/1732-Find-the-Highest-Altitude.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn largest_altitude(gain: Vec<i32>) -> i32 {
+        let mut current = 0;
+        let mut max_num = 0;
+
+        for element in gain {
+            println!("{current}, {element}");
+            current += element;
+            if current > max_num {
+                max_num = current;
+            }
+        }
+        return max_num;
+    }
+}

16a6d3b4fd03298ac723f266f03ce40730d9821f
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2024-09-25 15:43:15 +0900

    Title: Counting Bits, Time Beats: 8.00%, Memory Beats: 88.25% - LeetPush

--- leetcode/338-Counting-Bits.py
+++ leetcode/338-Counting-Bits.py
@@ -2,5 +2,5 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         answer = [0]
         for i in range(1, n + 1):
-            answer.append(sum(map(int, list(format(i, 'b')))))
+            answer.append(sum(map(int, bin(i)[2:])))
         return answer

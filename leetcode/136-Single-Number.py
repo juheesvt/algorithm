61610a96860ce1e1c2583327989824a04d8403f9
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2024-10-03 14:29:49 +0900

    Title: Single Number, Time Beats: 73.28%, Memory Beats: 98.69% - LeetPush

--- leetcode/136-Single-Number.py
+++ leetcode/136-Single-Number.py
@@ -1,14 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        counter = dict()
+        xor = 0
         for num in nums:
-            if num not in counter:
-                counter[num] = 0
-            counter[num] += 1
-        
-        for key, val in counter.items():
-            if val == 1:
-                return key
+            xor ^= num
+        return xor
+
         
         
         
